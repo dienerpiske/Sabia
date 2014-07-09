1a276fbbe4ba92e7192b06ebe6bf0b97b033bd46
Enza Rafaela <rafaeladenadai@gmail.com>
2014-07-08 23:58:52 -0300

    08072014

--- Sabia/urls.py
+++ Sabia/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     
     url(r'^inicio/$', 'SabiaApp.views.inicio', name='inicio'),
     url(r'^main_page/$', 'SabiaApp.views.main_page', name='main_page'),
-    url(r'^novo_fichamento/$', 'SabiaApp.views.novo_fichamento', name='novo_fichamento'),
+    url(r'^novo_fichamento/(?P<id_artigo>\d+)/$', 'SabiaApp.views.novo_fichamento', name='novo_fichamento'),
     url(r'^salvar_questao/$', 'SabiaApp.views.salvar_questao', name='salvar_questao'),
+    url(r'^registra_novo/$', 'SabiaApp.views.registra_novo', name='registra_novo'),
+    url(r'^insere_artigo/$', 'SabiaApp.views.insere_artigo', name='insere_artigo'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- SabiaApp/views.py
+++ SabiaApp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render_to_response
 from SabiaApp.models import *
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
-
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 class FichamentoModelForm(ModelForm):
     class Meta:
@@ -16,8 +16,9 @@ def inicio(request):
 def main_page(request):  
     return render_to_response('main_page.html')
 
-def novo_fichamento(request):
-    return render_to_response('novo_fichamento.html')
+def novo_fichamento(request, id_artigo):
+    artigo = Artigo.objects.get(id = id_artigo)
+    return render_to_response('novo_fichamento.html',{'artigo' : artigo})
 
 @csrf_exempt 
 def salvar_questao(request):
@@ -27,6 +28,23 @@ def salvar_questao(request):
         doc = Fichamento()
         doc.titulo_topico = request.POST['formQuestao']
         doc.usuario_id = '1'
-        doc.artigo_id = '1'      
-        doc = doc.save()       
-        return render_to_response('novo_fichamento.html',doc)
+        doc.artigo_id = request.POST['idOculto']      
+        doc.save()       
+        return HttpResponseRedirect(reverse('SabiaApp.views.novo_fichamento', args=[request.POST['idOculto']]))
+    #return render_to_response('novo_fichamento.html',doc)
+
+@csrf_exempt
+def registra_novo(request):    
+    art = {}
+    art.update(csrf(request))
+    
+    if request.POST:        
+        artigo = Artigo()
+        artigo.texto_artigo = request.POST['formArtigo']
+        artigo.titulo_artigo = request.POST['formTitulo']             
+        artigo.save()       
+            
+        return HttpResponseRedirect(reverse('SabiaApp.views.novo_fichamento', args=[artigo.id]))
+
+def insere_artigo(request):
+    return render_to_response('insere_artigo.html')
