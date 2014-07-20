f04da8025f3a8a4b20c0517a4bf5dc28f349bcd4
dienerpiske <dienermaickpiske@gmail.com>
2014-07-20 15:10:41 -0300

    Criando arquivo url dentro da aplicação. Alguns ajustes de template.

--- Sabia/urls.py
+++ Sabia/urls.py
@@ -7,17 +7,8 @@ admin.autodiscover()
 dajaxice_autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Sabia.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    (r'^sabia/', include('SabiaApp.urls')),
     (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
-    url(r'^inicio/$', 'SabiaApp.views.inicio', name='inicio'),
-    url(r'^home/$', 'SabiaApp.views.home', name='home'),
-    url(r'^novo_fichamento/(?P<id_artigo>\d+)/$', 'SabiaApp.views.novo_fichamento', name='novo_fichamento'),
-    url(r'^salvar_questao/$', 'SabiaApp.views.salvar_questao', name='salvar_questao'),
-    url(r'^registra_novo/$', 'SabiaApp.views.registra_novo', name='registra_novo'),
-    url(r'^novo_artigo/$', 'SabiaApp.views.novo_artigo', name='novo_artigo'),
-    url(r'^mostra_artigo/(?P<id_artigo>\d+)/$', 'SabiaApp.views.mostra_artigo', name='mostra_artigo'),
     url(r'^admin/', include(admin.site.urls)),
     
 )
---
+++ SabiaApp/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, patterns
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Sabia.views.home', name='home'),
+    url(r'^inicio/$', 'SabiaApp.views.inicio', name='inicio'),
+    url(r'^home/$', 'SabiaApp.views.home', name='home'),
+    url(r'^novo_fichamento/(?P<id_artigo>\d+)/$', 'SabiaApp.views.novo_fichamento', name='novo_fichamento'),
+    url(r'^salvar_questao/$', 'SabiaApp.views.salvar_questao', name='salvar_questao'),
+    url(r'^registra_novo/$', 'SabiaApp.views.registra_novo', name='registra_novo'),
+    url(r'^novo_artigo/$', 'SabiaApp.views.novo_artigo', name='novo_artigo'),
+    url(r'^mostra_artigo/(?P<id_artigo>\d+)/$', 'SabiaApp.views.mostra_artigo', name='mostra_artigo'),
+
+    
+)
