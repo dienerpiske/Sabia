1191a67e43a2d9ae2dd5745285164ee72aea407a
dienerpiske <dienermaickpiske@gmail.com>
2014-07-21 19:08:54 -0300

    Tradução usando API do Bing translator implementada. Necessario instalar modulo httplib2

--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Regras
 1. Somente commitar versões funcionais do projeto.
 2. Ao Commitar comentar informando todas as modificações e implementações realizadas.
 
+Prerequisito
+====
+1. django 1.6.5
+2. django-dajaxice.
+3. detectlanguages
+4. httplib2
+
+
 The Package Team
 ====
 Code 4Love (S2)
--- SabiaApp/ajax.py
+++ SabiaApp/ajax.py
@@ -1,12 +1,26 @@
+import detectlanguage
+import re
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 from SabiaApp.models import Artigo
 from SabiaApp.models import Fichamento
 from django.template.loader import render_to_string
+from SabiaApp.bing import MicrosoftTranslatorClient
+
 
 @dajaxice_register
 def sayhello(request):
-    return simplejson.dumps({'message':'Hello World'})
+    return simplejson.dumps({'message' : 'message from server'})
+
+@dajaxice_register
+def traduzir(request, texto, de, para):
+    detectlanguage.configuration.api_key = "f2d7fc33a7f3cb0b045d83b6a4c36ad9"
+    msg = detectlanguage.simple_detect("Buenos dias senor")
+    #translated_word = re.search("translatedText\":\"(.*)\"", ob.text).group(1)
+    client = MicrosoftTranslatorClient('sabialiedufes', 'HyJ6rFaIeEPd9q7ZjrWaixrKIUuAXuxIAroGL6YpRl8=')
+    t =  client.TranslateText(texto, de, para)
+
+    return simplejson.dumps({'texto' : t})
 
 @dajaxice_register
 def searchfichamentos(request, busca):
