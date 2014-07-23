fca5c80bc5dc8ea9475d1971be14fffbac265d46
dienerpiske <dienermaickpiske@gmail.com>
2014-07-22 23:08:15 -0300

    Ajustes feitos. The bird is flying. So sweet.

--- SabiaApp/ajax.py
+++ SabiaApp/ajax.py
@@ -9,6 +9,7 @@ from SabiaApp.bing import MicrosoftTranslatorClient
 
 @dajaxice_register
 def criarmarcacao(request, id_fichamento,titulo,texto):
+    print("1")
     fichamento = Fichamento.objects.get(id=id_fichamento)
     marcacao = Marcacao(titulo_marcacao = titulo, texto_marcacao = texto, fichamento = fichamento)
     marcacao.save()
@@ -58,3 +59,16 @@ def searchartigos(request, busca):
     listgroup = render_to_string('div_lg_artigo.html', {'artigos': artigos})
     return simplejson.dumps({'listgroup': listgroup})
 
+@dajaxice_register
+def salvartitulofichamento(request, id_f, titulo_f):
+    fichamento = Fichamento.objects.get(id=id_f)
+    fichamento.titulo_fichamento = titulo_f
+    fichamento.save()
+    
+@dajaxice_register
+def gostarfichamento(request, id_f):
+    fichamento = Fichamento.objects.get(id=id_f)
+    likes = fichamento.likes_fichamento + 1
+    fichamento.likes_fichamento = likes
+    fichamento.save()
+    return simplejson.dumps({'likes': likes})
