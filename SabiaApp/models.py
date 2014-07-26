1a9efc41897a56e8b59ad16ad0a626669bf4479c
SabrinaPanceri <sabrinapanceri@gmail.com>
2014-07-26 13:44:16 -0300

    Alterações realizadas meu_sabia.html = retirei o espaço depois de html mostra_artigo.html = retirei o espaço depois de html novo_artigo.html = retirei o espaço depois de html novo_fichamento.html = retirei o espaço depois de html
    arquivo: models.py
    -> criação da função getTitulosSemelhantes
    - retorna os artigos cadastrados que possuem o título semelhante ao na
    pesquisa. Usa o algoritmo Cosine para o calculo da similaridade entre as
    strings.
    
    -> criação da função palavrasChaves
    - Trata o texto eliminando as stopwords
    - Quantifica a frequencia de cada termo
    - Apresenta ao usuário os quatro termos com maior frequencia
    - Utiliza funções da biblioteca NLTK
    
    arquivo: similaridade.py
    -> desenvolvimento da algoritmo de Similaridade de Co-senos
    
    arquivo:mostra_artigo.html
    -> inserção de um novo div chamado "Termos frequentes"
    - É alimentado pela função palavrasChaves
    
    arquivo: db.testebanco.dql
    -> inserção de dados reais para teste do algoritmo
    
    OBS: O algoritmo foi desenvolvido para análise de textos em geral,
    contudo se for analisar textos em português é necessário alterar a
    atribuição da variável stop no arquivo models.py (isso está comentado no
    código tbm!)

--- SabiaApp/models.py
+++ SabiaApp/models.py
@@ -2,6 +2,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Import's necessarios
+from nltk.corpus import stopwords
+from nltk import FreqDist
+from similarity import similarity, vetores
+
 class Usuario(models.Model):
     user = models.OneToOneField(User)
     informacoes_usuario = models.TextField(max_length = 500)
@@ -13,12 +18,49 @@ class Artigo(models.Model):
     titulo_artigo = models.TextField()
     texto_artigo = models.TextField()
     tags_artigo = models.TextField()
-    
+        
     class Meta:
         db_table = 'Artigo'
+    
+    def getTitulosSemelhantes(self):
+        artigosBD = Artigo.objects.all()
+        artigosSim = []
         
-    def getArtSemelhantes(self):
-        return Artigo.objects.all() #mudar isso usando o comparados por tag
+        titulo1 = self.titulo_artigo.lower()
+        
+        aux1 = vetores(titulo1)
+
+        for  i in range(0,len(artigosBD)):            
+            titulo2 = artigosBD[i].titulo_artigo.lower()
+            aux2 = vetores(titulo2)
+            
+            if self.id != artigosBD[i].id and similarity(aux1, aux2):
+                artigosSim.append(artigosBD[i])
+                
+        return artigosSim
+    
+    def palavrasChaves(self):
+        stop = stopwords.words('english')
+#         para analisar textos em portugues, habilitar essa linha e comentar a de cima
+#         stop = stopwords.words('portuguese')
+        palavrasChaves = [] 
+        
+        #retira as stopwords do texto do artigo que está sendo apresentado
+        #retira a ',' e '.' do texto
+        textoArtigo =  [i for i in self.texto_artigo.lower().replace(',','').replace('.','').split() if i not in stop]
+        
+        #apresenta a frequencia de repetição das palavras no corpo do artigo
+        freq = FreqDist(textoArtigo)
+        
+        #separa as quatro palavras mais frequentes
+        items = freq.most_common(4)
+        
+        for i in range(0,len(items)):
+            palavrasChaves.append(items[i][0].upper())
+            
+        return palavrasChaves
+        
+    
 
 class Fichamento(models.Model):
     titulo_fichamento = models.TextField()
